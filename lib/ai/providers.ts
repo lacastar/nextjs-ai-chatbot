cf438387f5c2f3bada6f7278e56df98e96cb1b3c
Laszlo Szenthe <lacus-github@rainstorm.org>
2025-03-11 15:54:22 +0100

    deepseek provider

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -3,8 +3,10 @@ import {
   extractReasoningMiddleware,
   wrapLanguageModel,
 } from 'ai';
-import { openai } from '@ai-sdk/openai';
-import { fireworks } from '@ai-sdk/fireworks';
+//import { openai } from '@ai-sdk/openai';
+//import { fireworks } from '@ai-sdk/fireworks';
+//import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { openrouter } from '@openrouter/ai-sdk-provider';
 import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
@@ -12,6 +14,10 @@ import {
   reasoningModel,
   titleModel,
 } from './models.test';
+
+/*const openrouter = createOpenRouter({
+  apiKey: 'YOUR_OPENROUTER_API_KEY',
+});*/
 
 export const myProvider = isTestEnvironment
   ? customProvider({
@@ -25,17 +31,18 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-        'chat-model-small': openai('gpt-4o-mini'),
-        'chat-model-large': openai('gpt-4o'),
-        'chat-model-reasoning': wrapLanguageModel({
-          model: fireworks('accounts/fireworks/models/deepseek-r1'),
-          middleware: extractReasoningMiddleware({ tagName: 'think' }),
-        }),
-        'title-model': openai('gpt-4-turbo'),
-        'artifact-model': openai('gpt-4o-mini'),
+        'deepseek-free': openrouter.chat('deepseek/deepseek-r1:free')
+        //'chat-model-small': openai('gpt-4o-mini'),
+        //'chat-model-large': openai('gpt-4o'),
+        //'chat-model-reasoning': wrapLanguageModel({
+        //  model: fireworks('accounts/fireworks/models/deepseek-r1'),
+        //  middleware: extractReasoningMiddleware({ tagName: 'think' }),
+        //}),
+        //'title-model': openai('gpt-4-turbo'),
+        //'artifact-model': openai('gpt-4o-mini'),
       },
-      imageModels: {
-        'small-model': openai.image('dall-e-2'),
-        'large-model': openai.image('dall-e-3'),
-      },
+      //imageModels: {
+      //  'small-model': openai.image('dall-e-2'),
+      //  'large-model': openai.image('dall-e-3'),
+      //},
     });
